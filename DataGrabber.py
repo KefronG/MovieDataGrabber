5aec9a79da93014953d8c77826461d5ad197b11b
Kefron <grantkefron@yahoo.com>
2022-12-30 17:58:15 -0500

    Updated url calls so I'm not spamming unceeralliy. Also made starring list smarter, it returns the actor with the highest popularity score and following actors if their score is higher than a set value based on highest score / dived my number.

--- DataGrabber.py
+++ DataGrabber.py
@@ -19,13 +19,10 @@ def my_movie_dictionary(mydir):
         tempSplit = titles_and_years[cntr - 1].split(')')[0]
         jst_title_and_year = tempSplit.split('(')
         titles_and_year_of_release.append(jst_title_and_year)
-
     return titles_and_year_of_release
 
 
 def get_age_rating(movie_id):
-
-    #url = f"https://api.themoviedb.org/3/movie/{movie_id}/release_dates?api_key={KeysandPaths.SECERT_KEY2}"
     url = f"https://api.themoviedb.org/3/find/{movie_id}?api_key={KeysandPaths.SECERT_KEY2}&external_source=imdb_id"
     response = requests.get(url)
     jsonresponse = response.json()
@@ -93,7 +90,7 @@ if __name__ == '__main__':
     imdb_ids = []
     movie_genres = []
     # len(my_movie_dictionary(KeysandPaths.rtnfolder_path()))
-    for i in range(len(my_movie_dictionary(KeysandPaths.rtnfolder_path()))):
+    for i in range(10):
         T = my_movie_dictionary(KeysandPaths.rtnfolder_path())[i][0]
         Y = my_movie_dictionary(KeysandPaths.rtnfolder_path())[i][1]
         print(f"{count}.", f"{T} {Y}")
@@ -108,10 +105,6 @@ if __name__ == '__main__':
         path_4_posters = KeysandPaths.rtnpath_4_posters(img_dir)
         #download_image(f"{path_4_posters} ", img_url,f"{T}{Y} poster.jpg")
 
-
-
-
-
         age_rating_certification = [rating for rating in get_age_rating(imdb_ids[i]) if rating["iso_3166_1"] == "US"]
         rating = []
         if age_rating_certification == []:
@@ -122,15 +115,6 @@ if __name__ == '__main__':
             if age_rating  == []:
                 rating.append('NR')
         print(f"Age rating: {rating[0]}\n")
-
-
-
-
-
-
-
-
-
 
         genres = getmovieinfo(imdb_ids[i])[0]['genres']
         print("Genres:")
@@ -146,9 +130,18 @@ if __name__ == '__main__':
         print(f"Viewer rating: {getmovieinfo(imdb_ids[i])[0]['vote_average']} \n")
 
         print("Starring: ")
-        print(getcredits(imdb_ids[i])[0]['cast'][0]['name'])
-        print(getcredits(imdb_ids[i])[0]['cast'][1]['name'])
-        print(getcredits(imdb_ids[i])[0]['cast'][2]['name'])
+        crew = getcredits(imdb_ids[i])[0]['cast']
+        crew_scores = [c for c in crew if crew[len(crew) - 1]['popularity']]
+        pop_score = []
+        for c in range(len(crew_scores)):pop_score.append(crew_scores[c]['popularity'])
+        pop_score.sort()
+        ajst_score = pop_score[len(pop_score)-1] / 4
+        cast = []
+        for c in range(len(crew)):
+            if  int(crew[c]['popularity']) >= int(ajst_score):
+                cast.append(crew[c])
+        for c in range(len(cast)): print(cast[c]['name'])
+
 
 
         directors = [credit for credit in getcredits(imdb_ids[i])[0]['crew'] if credit["job"] == "Director"]
