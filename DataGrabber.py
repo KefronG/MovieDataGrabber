cb69d226211f3652e21a10e2d2ad359f06d4f941
Kefron <grantkefron@yahoo.com>
2022-12-31 15:56:40 -0500

    Used more for loops to reduces the number of request I was making.

--- DataGrabber.py
+++ DataGrabber.py
@@ -5,7 +5,6 @@ import KeysandPaths
 from PIL import Image
 import io
 
-
 def my_movie_dictionary(mydir):
     titles_and_years = []
     titles_and_year_of_release = []
@@ -79,29 +78,37 @@ def write_movie_overview(download_path, overview, file_name):
 
 
 
-
-
 if __name__ == '__main__':
     print(f'Total Movies in Home Movies Folder {len(my_movie_dictionary(KeysandPaths.rtnfolder_path()))} \n')
     count = 1
     imdb_ids = []
-    movie_genres = []
+
+
     # len(my_movie_dictionary(KeysandPaths.rtnfolder_path()))
+    #For each Movie In my home movies folder get:
     for i in range(10):
+
+        #Title and Year
         T = my_movie_dictionary(KeysandPaths.rtnfolder_path())[i][0]
         Y = my_movie_dictionary(KeysandPaths.rtnfolder_path())[i][1]
         print(f"{count}.", f"{T} {Y}")
         ID = get_imdb_movie_poster_and_id(T, Y)
 
+
+        #IMDB ID
         print(f"IMDB ID: {ID[0]['id']}")
         imdb_ids.append(ID[0]['id'])
 
+
+        #Posters
         print(f"Movie Poster: {ID[0]['image']} \n")
         img_url = ID[0]['image']
         img_dir = T + "("+Y+")"
         path_4_posters = KeysandPaths.rtnpath_4_posters(img_dir)
         #download_image(f"{path_4_posters} ", img_url,f"{T}{Y} poster.jpg")
 
+
+        #Age rating
         age_rating_certification = [rating for rating in get_age_rating(imdb_ids[i]) if rating["iso_3166_1"] == "US"]
         rating = []
         if age_rating_certification == []:
@@ -113,35 +120,49 @@ if __name__ == '__main__':
                 rating.append('NR')
         print(f"Age rating: {rating[0]}\n")
 
+
+        #Genres
+        movie_genres = []
         genres = getmovieinfo(imdb_ids[i])[0]['genres']
         print("Genres:")
-        for g in range(len(genres)): print(f"{genres[g]['name']}")
+        for g in range(len(genres)): movie_genres.append(genres[g]['name'])
+        for g in range(len(genres)):print(movie_genres[g])
 
+
+        #Overview
         overview = getmovieinfo(imdb_ids[i])[0]['overview']
         print(f"\nOverview: {overview} \n")
         overview_dir = T + "(" + Y + ")"
         path_4_overview = KeysandPaths.rtnpath_4_overview(overview_dir)
         #write_movie_overview(f"{path_4_overview}",overview,f"{T}{Y} overview.txt" )
 
+        #Runtime
         print(f"Runtime: {getmovieinfo(imdb_ids[i])[0]['runtime']} \n")
+
+        #Viewer rating
         print(f"Viewer rating: {getmovieinfo(imdb_ids[i])[0]['vote_average']} \n")
 
+
+        #starring cast
         print("Starring: ")
         crew = getcredits(imdb_ids[i])[0]['cast']
         crew_scores = [c for c in crew if crew[len(crew) - 1]['popularity']]
         pop_score = []
         for c in range(len(crew_scores)):pop_score.append(crew_scores[c]['popularity'])
         pop_score.sort()
-        ajst_score = pop_score[len(pop_score)-1] / 4
+        ajst_score = pop_score[len(pop_score)-1] / 2
         cast = []
         for c in range(len(crew)):
             if  int(crew[c]['popularity']) >= int(ajst_score):
                 cast.append(crew[c])
         for c in range(len(cast)): print(cast[c]['name'])
 
-        directors = [credit for credit in getcredits(imdb_ids[i])[0]['crew'] if credit["job"] == "Director"]
 
-        for people in range(len(directors)): print(f"\nDirector: {directors[people]['name']}")
+        #Directors
+        Directors =[]
+        lst_directors = [credit for credit in getcredits(imdb_ids[i])[0]['crew'] if credit["job"] == "Director"]
+        for people in range(len(lst_directors)): Directors.append(lst_directors[people]['name'])
+        for name in range(len(lst_directors)):print(f"\nDirector: {Directors[name]}")
 
 
         print("____________________________________________________________________________________________________\n")
